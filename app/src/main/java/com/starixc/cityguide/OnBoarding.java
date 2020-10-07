bcf0c308522c4c0479882e458bc5596d2e308e5b
Chris Erricks <starixc@gmail.com>
2020-10-07 14:31:41 +0300

    City Guide Up Onboarding screen build

---
+++ app/src/main/java/com/starixc/cityguide/OnBoarding.java
@@ -0,0 +1,21 @@
+package com.starixc.cityguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class OnBoarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+
+    public void skip(View view) {
+    }
+
+    public void next(View view) {
+    }
+}
---
+++ app/src/main/java/com/starixc/cityguide/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.starixc.cityguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+    ImageView image;
+    TextView logo,slogan;
+//    Animation topAnim,bottomAnim;
+protected int SPLASH_SCREEN = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        image = findViewById(R.id.imageView);
+        logo= findViewById(R.id.textView);
+        slogan=findViewById(R.id.sloganTxt);
+//        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+//        bottomAnim=AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+//        image.setAnimation(topAnim);
+//        logo.setAnimation(bottomAnim);
+
+        super.onCreate(savedInstanceState);
+//        remove the status bar from the splash screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
