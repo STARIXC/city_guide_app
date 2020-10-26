4bb84cff3018a97c69168ae09f84a0120f3d23e5
Chris Erricks <starixc@gmail.com>
2020-10-26 11:44:07 +0300

    Category Layout update

---
+++ app/src/main/java/com/starixc/cityguide/User/AllCategories.java
@@ -0,0 +1,16 @@
+package com.starixc.cityguide.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.starixc.cityguide.R;
+
+public class AllCategories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_categories);
+    }
+}
